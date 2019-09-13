b07c90b63f75ef07d62994e299bf18f1a5a2ef03
luisas <luisa.santus95@gmail.com>
2019-09-13 17:37:17 +0200

    data preparation intermediate step

--- src/data_preparation.nf
+++ src/data_preparation.nf
@@ -24,7 +24,7 @@ process merge_assemblies {
     file ebov from ebov_genome_channel
 
     output:
-    file "rheMac8_EBOV-Kikwit.fa" into merged_assembly
+    file "rheMac8_EBOV-Kikwit.fa" into (merged_assembly, merged_assembly_for_dictionary )
 
     script:
     """
@@ -33,3 +33,126 @@ process merge_assemblies {
     """
 
 }
+
+
+
+// /*
+// * Create hisat2 indexes
+// *
+// * Build hisat2 indexes for joined rheMac8 and EBOV assemblies specifying
+// * exon and splice-sites files as done in the Tuxedo Protocol paper 2016
+// *
+// */
+// process create_hisat2_indexes{
+//
+//   storeDir "${params.output_dir}/indexes/hisat2"
+//
+//   input:
+//   file assembly from merged_assembly
+//
+//   output:
+//   file "*" into hisat2_indexes
+//
+//   script:
+//   """
+//   hisat2-build-s ${assembly} .
+//   """
+// }
+//
+//
+// /*
+// * Create dictionary for merged assembly
+// */
+// process create_dictionary{
+//
+//   storeDir "${params.output_dir}/indexes/hisat2"
+//
+//   input:
+//   file assembly from merged_assembly_for_dictionary
+//
+//   output:
+//   file "*.dict" into hisat2_indexes
+//
+//   script:
+//   """
+//   java -Xmx4g  -Djava.io.tmpdir=$TMPDIR -jar /apps/PICARD/2.20.0/picard.jar CreateSequenceDictionary R=${assembly}
+//   """
+// }
+
+
+/*
+* REQUIRED same chrom names in reference assembly and gene annotations
+* Replace ensembl names with ucsc names
+*/
+params.rheMac8_annotation_gz_file = "/gpfs/projects/bsc83/gene_annotation/ensembl_release97/rheMac8/Macaca_mulatta.Mmul_8.0.1.97.gtf.gz"
+rheMac8_annotation_gz_channel = Channel
+                                  .fromPath("${params.rheMac8_annotation_gz_file}")
+                                  .map{it.baseName.minus(".gtf.gz"),it)))}
+params.chromAlias = "/gpfs/projects/bsc83/Ebola/00_InformationFiles/gene_annotations/chromAlias.txt" 
+chromAliasChannel = Channel.fromPath("${params.chromAlias}")
+
+process parse_identifiers{
+  storeDir "${params.output_dir}/gene_annotations"
+
+  input:
+  file chromAlias from chromAliasChannel
+
+  output:
+  file "ensembl.ucsc.chrom_names_correspondance.tab" into ensembl_correspondence
+
+  script:
+  """
+  #!/usr/bin/R
+  chromAlias=read.table(${chromAlias})
+  contigs <- chromAlias[chromAlias$V3=="refseq",2]
+  x1=chromAlias[sapply(chromAlias$V3, function(i) "ensembl" %in% i),]
+  x2=chromAlias[sapply(chromAlias$V3, function(i) "genbank,ensembl" %in% i),]
+  z=rbind.data.frame(x1,x2)[,c(1:2)]
+  write.table(z,"ensembl.ucsc.chrom_names_correspondance.tab",col.names=F, row.names=F,sep="\t",quote=F)
+  """
+}
+
+
+process gene_annotation{
+
+  storeDir "${params.output_dir}/gene_annotations"
+
+  input:
+  set  annotation_name, file(rheMac8_annotation_gz) from rheMac8_annotation_gz_channel
+  file ensembl_correspondence from ensembl_correspondence_channel
+
+  output:
+  set file("rheMac8.ensembl_release97.gtf"), file("rheMac8.ensembl_release97.tab") into gene_annotations
+
+  script:
+  """
+  # Replace ensembl names with ucsc names -> REQUIRED same chrom names in reference assembly and gene annotations
+  awk 'FNR==NR {x2[$1] = $2; next} $1 in x2 {OFS="\t";$1=x2[$1];print $0}' ${ensembl_correspondence}  <(zcat ${rheMac8_annotation_gz}) | cut -f1-8 > ${annotation_name}.column1-8.ucsc_chrom_names.txt
+  # Create one file with description column
+  zcat ${rheMac8_annotation_gz} | cut -f9 | awk '{if($1~/^#/){next}else{print}}' > ${annotation_name}.column9.txt
+  # Complete parsing
+  cat <(zcat  ${rheMac8_annotation_gz} | awk '{if($1~/^#/)print}') <( paste ${annotation_name}.column1-8.ucsc_chrom_names.txt ${annotation_name}.column9.txt) > rheMac8.ensembl_release97.gtf
+  # Parse to tab delimited
+  cat rheMac8.ensembl_release97.gtf | ./gtf2bed.py > rheMac8.ensembl_release97.tab
+  """
+}
+
+
+// /*
+// * to be done when annotations are ready
+// */
+// process hisat2_ss_exon{
+//
+//   storeDir "${params.output_dir}/indexes/hisat2"
+//
+//   input:
+//   file assembly from merged_assembly_for_dictionary
+//
+//   output:
+//   file "*.dict" into hisat2_indexes
+//
+//   script:
+//   """
+//   hisat2-build -p 8 --ss /gpfs/projects/bsc83/Ebola/indexes/hisat2_ss_exon/rheMac8_EBOV-Kikwit.ss.txt  --exon /gpfs/projects/bsc83/Ebola/indexes/hisat2_ss_exon/rheMac8_EBOV-Kikwit.exon.txt /gpfs/projects/bsc83/Ebola/indexes/hisat2_ss_exon/rheMac8_EBOV-Kikwit.fa /gpfs/projects/bsc83/Ebola/indexes/hisat2_ss_exon/rheMac8_EBOV-Kikwi
+//   """
+// }
--- src/scripts/fastqc_results.heatmap.R
+++ src/scripts/fastqc_results.heatmap.R
@@ -12,12 +12,7 @@ options(stringsAsFactors = F)
 # ------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # Variables
-inpath="/Users/luisasantus/Desktop/mn_cluster/mount_dirs/projects/02_fastqc/Zyagen_samples/"
-sample_info=read.table("/Users/luisasantus/Desktop/mn_cluster/mount_dirs/projects/00_InformationFiles/sample_info.Zyagen_test.txt")
-colnames(sample_info)<-c("fastq_file","path",
-                         "sample","tissue",
-                         "lane","method",
-                         "stranded")
+inpath="/Users/luisasantus/Desktop/mn_cluster/mount_dirs/projects/02_fastqc/Zyagen_samples"
 
 # FastqQC results column names
 hm_colnames<-c("Basic Statistics",
@@ -33,28 +28,31 @@ hm_colnames<-c("Basic Statistics",
                "Adapter Content","Kmer Content")
 
 res_fastqc<-list()
-for(row in 1:nrow(sample_info)){
-  # r1
-  # Creates path to fastqc folder
-  fastq_summary<-paste0(inpath,sample_info[row,"tissue"],"/",sample_info[row,"sample"],"/",
-                        sample_info[row,"fastq_file"],".1_fastqc/summary.txt")
-  qc_res<-read.delim(fastq_summary,sep="\t",header = F)
-  qc_res$number<-as.numeric(gsub("FAIL",0,gsub("WARN",1,gsub("PASS",2,qc_res$V1))))
-  sample_data_r1<-c(sample_info[row,"tissue"],
-                    sample_info[row,"sample"],
-                    sample_info[row,"lane"],
-                    "R1",
-                    ifelse(sample_info[row,"tissue"]=="Blood",sample_info[row,"method"],"NA"),
-                    qc_res$number)
-  sample_data_r2 <- sample_data_r1
-  sample_data_r1[4] <- "R2"
+files <- list.files(path=inpath, pattern="summary.txt", full.names=TRUE, recursive=TRUE)
+
+for(file in unique(gsub("*.\\d_fastqc/summary.txt", "", files))){
+  info <- strsplit(strsplit(file,"/", fixed = T)[[1]][12],".", fixed = T)[[1]]
+ 
+  print(fastq_summary_1)
+  fastq_summary_1<-paste0(file,".1_fastqc/summary.txt")
+  qc_res_1<-read.delim(fastq_summary_1,sep="\t",header = F)
+  qc_res_1$number<-as.numeric(gsub("FAIL",0,gsub("WARN",1,gsub("PASS",2,qc_res_1$V1))))
+  sample_data_r1<-c(info[1],info[2],info[3],"R1",qc_res_1$number)
+  print(qc_res_1)
+  fastq_summary_2<-paste0(file,".2_fastqc/summary.txt")
+  qc_res_2<-read.delim(fastq_summary_2,sep="\t",header = F)
+  qc_res_2$number<-as.numeric(gsub("FAIL",0,gsub("WARN",1,gsub("PASS",2,qc_res_2$V1))))
+  sample_data_r2<-c(info[1],info[2],info[3],"R2",qc_res_2$number)
+  
   res_fastqc[[length(res_fastqc)+1]]<-sample_data_r1
   res_fastqc[[length(res_fastqc)+1]]<-sample_data_r2
 }
 
+
+res_fastqc
 res_fastqc_df<-do.call(cbind.data.frame,res_fastqc)
 colnames(res_fastqc_df)<-""
-rownames(res_fastqc_df)<-c("Tissue","Sample","Lane","ReadPair","Method",hm_colnames)
+rownames(res_fastqc_df)<-c("Tissue","Sample","Lane","ReadPair",hm_colnames)
 
 
 
@@ -65,10 +63,8 @@ qc_cols<-c("#008744","#ffa700","#d62d20")
 names(qc_cols)<-c(2,1,0)
 
 # Tissue Colors
-tissue_cols<-c("#a30041","#468499")
-names(tissue_cols)<-c("Adrenal","Brain")
-# tissue_cols<-c("#2B061E","#775253", "#4392F1","#7180AC", "#2B4570","#8AEA92", "#F6E27F","#E2C391", "#EB5160", "#7E52A0" )
-# names(tissue_cols)<-c("Adrenal","Brain","Kidney","LN", "Liver","Ovary","Skin","SpinalCord","Spleen", "Testis")
+tissue_cols<-c("#2B061E","#775253", "#4392F1","#7180AC", "#2B4570","#8AEA92", "#F6E27F","#E2C391", "#EB5160", "#7E52A0" )
+names(tissue_cols)<-c("Adrenal","Brain","Kidney","LN", "Liver","Ovary","Skin","SpinalCord","Spleen", "Testis")
 
 # Sample colors
 samples<-unique(as.factor(as.character(res_fastqc_df["Sample",])))
@@ -76,7 +72,7 @@ if (length(samples)  == 1){
   sample_cols <- c("#FFA552")
   names(sample_cols)<-levels(samples)
 }else if(length(samples)  == 2) { 
-  sample_cols <- c("#FFA552", "381D2A")
+  sample_cols <- c("#0000A0", "#ADD8E6")
   names(sample_cols)<-levels(samples)
 }else{
   sample_cols<-brewer.pal(length(samples),"Paired")
@@ -87,25 +83,7 @@ if (length(samples)  == 1){
 readpair_cols<-brewer.pal(3,"Set1")[c(2:3)]
 names(readpair_cols)<-c("R1","R2")
 
-# Lane Colors
-lanes<-unique(as.factor(as.character(res_fastqc_df["Lane",])))
-lanes_cols<-c("#bada55","#ff6666")
-names(lanes_cols)<-c("1","2")
 
-# Metadata Colors
-metadata<-unique(as.factor(as.character(res_fastqc_df["Method",])))
-length(metadata)
-
-if (length(metadata)  == 1){ 
-  metadata_cols <- c("#EAC435")
-  names(metadata_cols)<-levels(metadata)
-}else if(length(metadata)  == 2) { 
-  metadata_cols <- c("#EAC435", "#E40066")
-  names(metadata_cols)<-levels(metadata)
-}else{
-  metadata_cols<-brewer.pal(length(metadata),"Dark2") # c("#FBB4AE","#B3CDE3","#CCEBC5","#DECBE4","#FED9A6","#FFFFCC","#E5D8BD","#FDDAEC")#
-  names(metadata_cols)<-metadata
-  }
 # ------------------------------------------------------------------------------------------------------------------------------------------------------
 
 ht <- Heatmap(as.matrix(res_fastqc_df[hm_colnames[-1],]),
@@ -118,39 +96,13 @@ ht <- Heatmap(as.matrix(res_fastqc_df[hm_colnames[-1],]),
                                                  Sample = as.factor(as.character(res_fastqc_df["Sample",])),
                                                  Lane = as.factor(as.character(res_fastqc_df["Lane",])),
                                                  ReadPair = as.factor(as.character(res_fastqc_df["ReadPair",])),
-                                                 Metadata = as.factor(as.character(res_fastqc_df["Method",])),
-                                                 col = list( Tissue = tissue_cols,
-                                                             Sample = sample_cols,
-                                                             Lane = lanes_cols,
-                                                             ReadPair = readpair_cols,
-                                                             Metadata = metadata_cols) ),
+                                                 col = list(Tissue = tissue_cols,
+                                                            Sample = sample_cols,
+                                                            ReadPair = readpair_cols),
+                                                 show_annotation_name = T),
               cluster_rows=T,cluster_columns = T,#d696bb
               show_column_names = F)
-png(paste0(inpath,"fastqc_resLuisa.png"),width = 900,height = 400)
-draw(ht)
-dev.off()
-
-ht <- Heatmap(as.matrix(res_fastqc_df[hm_colnames[-1],]),
-              col=qc_cols,
-              name="",
-              heatmap_legend_param = list(
-                at = c(2,1,0),
-                labels = c("PASS", "WARN", "FAIL")),
-              top_annotation = HeatmapAnnotation(Tissue = as.factor(as.character(res_fastqc_df["Tissue",])),
-                                                 Sample = as.factor(as.character(res_fastqc_df["Sample",])),
-                                                 Lane = as.factor(as.character(res_fastqc_df["Lane",])),
-                                                 ReadPair = as.factor(as.character(res_fastqc_df["ReadPair",])),
-                                                 Metadata = as.factor(as.character(res_fastqc_df["Method",])),
-                                                 col = list( Tissue = tissue_cols,
-                                                             Sample = sample_cols,
-                                                             Lane = lanes_cols,
-                                                             ReadPair = readpair_cols,
-                                                             Metadata = metadata_cols),
-                                                 show_annotation_name = T),
-              cluster_rows=F,cluster_columns = F,#d696bb
-              show_column_names = F)
-#column_title = "No clustering")
-png(paste0(inpath,"fastqc_res.png"),width = 900,height = 400)
+png(paste0(inpath,"/fastqc_res.png"),width = 900,height = 400)
 draw(ht)
 dev.off()
 
