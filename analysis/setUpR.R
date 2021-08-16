ac955e54b931c5e3bd748183998eae49834dfcbf
luisa <luisa.santus@bsc.es>
2021-08-16 17:12:25 +0200

    AnalysisCodeClean

--- analysis/setUpR.R
+++
@@ -1,7 +0,0 @@
-
-if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-
-BiocManager::install(c( "rtracklayer", "stringr","ggplot2", "grid", "gridExtra", "RColorBrewer", "readr", "matrixStats",
-                            "GenomicRanges", "dplyr", "zeallot", "tximport", "ggpubr", "plyr",
-                            "formattable" ))
